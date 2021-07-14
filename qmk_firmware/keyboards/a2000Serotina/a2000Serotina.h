28404b27918d1daa7de925c06ce9c5d8302228d9
grahamshaw1972 <35458400+grahamshaw1972@users.noreply.github.com>
2021-07-14 18:55:05 +0100

    Updated column / row layout of switches
    Now much more regular on the bottom rows.

--- qmk_firmware/keyboards/a2000Serotina/a2000Serotina.h
+++ qmk_firmware/keyboards/a2000Serotina/a2000Serotina.h
@@ -65,36 +65,36 @@ unsigned long get_msec(void);
 #define LAMI B6
 #define CTRL B3
 #define RAMI B2
-/*
+
 #define LAYOUT( \
-    K00,      K02, K03, K04, K05,      K07, K08, K09,        \
-    K10,      K12, K13, K14, K15,      K17, K18, K19,        \
+    K00,      K02, K03, K04, K05, K06, K07, K08, K09, K0A,   \
+    K10, K11, K12, K13, K14, K15,                            \
     K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A,   \
     K30, K31, K32, K33, K34, K35, K36, K37, K38, K39,        \
     K40, K41, K42, K43, K44, K45, K46, K47, K48, K49, K4A,   \
     K50, K51, K52, K53, K54, K55, K56, K57, K58, K59,        \
-    K60, K61, K62, K63, K64, K65, K66, K67,                  \
-    K70, K71, K72, K73, K74, K75, K76, K77,                  \
-    K80, K81, K82, K83, K84, K85, K86, K87, K88,             \
-    K90, K91, K92, K93, K94, K95, K96, K97, K98,             \
-    KA0, KA1, KA2, KA3, KA4, KA5, KA6,                       \
-    KB0, KB1, KB2, KB3, KB4, KB5                             \
+    K60, K61, K62, K63, K64, K65, K66, K67, K68, K69, K6A,   \
+    K70, K71,                     K76, K77, K78,             \
+    K80, K81, K82, K83, K84, K85, K86, K87, K88, K89, K8A,   \
+    K90, K91,           K94,      K96, K97, K98, K99,        \
+    KA0, KA1, KA2,                KA6,                KAA,   \
+    KB0, KB1, KB2, KB3, KB4, KB5, KB6,      KB8              \
 ) { \
-    { K00,   KC_NO, K02,   K03,   K04,   K05,   KC_NO, K07,   K08,   K09,   KC_NO }, \
-    { K10,   KC_NO, K12,   K13,   K14,   K15,   KC_NO, K17,   K18,   K19,   KC_NO }, \
+    { K00,   KC_NO, K02,   K03,   K04,   K05,   K06,   K07,   K08,   K09,   K0A   }, \
+    { K10,   K11,   K12,   K13,   K14,   K15,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO }, \
     { K20,   K21,   K22,   K23,   K24,   K25,   K26,   K27,   K28,   K29,   K2A   }, \
     { K30,   K31,   K32,   K33,   K34,   K35,   K36,   K37,   K38,   K39,   KC_NO }, \
     { K40,   K41,   K42,   K43,   K44,   K45,   K46,   K47,   K48,   K49,   K4A   }, \
     { K50,   K51,   K52,   K53,   K54,   K55,   K56,   K57,   K58,   K59,   KC_NO }, \
-    { K60,   K61,   K62,   K63,   K64,   K65,   K66,   K67,   KC_NO, KC_NO, KC_NO }, \
-    { K70,   K71,   K72,   K73,   K74,   K75,   K76,   K77,   KC_NO, KC_NO, KC_NO }, \
-    { K80,   K81,   K82,   K83,   K84,   K85,   K86,   K87,   K88,   KC_NO, KC_NO }, \
-    { K90,   K91,   K92,   K93,   K94,   K95,   K96,   K97,   K98,   KC_NO, KC_NO }, \
-    { KA0,   KA1,   KA2,   KA3,   KA4,   KA5,   KA6,   KC_NO, KC_NO, KC_NO, KC_NO }, \
-    { KB0,   KB1,   KB2,   KB3,   KB4,   KB5,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO }, \
+    { K60,   K61,   K62,   K63,   K64,   K65,   K66,   K67,   K68,   K69,   K6A   }, \
+    { K70,   K71,   KC_NO, KC_NO, KC_NO, KC_NO, K76,   K77,   K78,   KC_NO, KC_NO }, \
+    { K80,   K81,   K82,   K83,   K84,   K85,   K86,   K87,   K88,   K89,   K8A   }, \
+    { K90,   K91,   KC_NO, KC_NO, K94,   KC_NO, K96,   K97,   K98,   K99,   KC_NO }, \
+    { KA0,   KA1,   KA2,   KC_NO, KC_NO, KC_NO, KA6,   KC_NO, KC_NO, KC_NO, KAA   }, \
+    { KB0,   KB1,   KB2,   KB3,   KB4,   KB5,   KB6,   KC_NO, KB8,   KC_NO, KC_NO }, \
 }
-*/
 
+/*
 #define LAYOUT( \
     K00, K01, K02, K03,   \
     K10, K11, K12, K13,   \
@@ -108,6 +108,7 @@ unsigned long get_msec(void);
     { K30,   K31,   K32,   KC_NO }, \
     { KC_NO, K41,   K42,   K43   }, \
 }
+*/
 
 // Amiga Keycodes
 #define AKC_RST_WARN 0x78
--- qmk_firmware/keyboards/a2000Serotina/keymaps/uk/keymap.c
+++ qmk_firmware/keyboards/a2000Serotina/keymaps/uk/keymap.c
@@ -17,27 +17,26 @@
 #include QMK_KEYBOARD_H
 #include "../../a2000Serotina.h"
 
-/*
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,            KC_F5,   KC_F6,   KC_F7,
-        KC_F8,            KC_F9,   KC_F10,  KC_F11,  KC_F12,           KC_PSCR, KC_SLCK, KC_PAUS,
+        KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,
+        KC_F10,  KC_F11,  KC_F12,  KC_PSCR, KC_SLCK, KC_PAUS,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
         KC_MINS, KC_EQL,  KC_BSPC, KC_INS,  KC_HOME, KC_PGUP, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
         KC_LBRC, KC_RBRC, KC_ENT,  KC_DEL,  KC_END,  KC_PGDN, KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
-        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,
-        KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_HASH, KC_P4,   KC_P5,   KC_P6,
-        KC_LSFT, KC_BSLS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,
-        KC_COMM, KC_DOT,  KC_SLSH, KC_LSFT, KC_UP,   KC_P1,   KC_P2,   KC_P3,   KC_PENT,
-        KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  KC_RALT, KC_RGUI, KC_MENU,
-        KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT, KC_INS,  KC_DEL
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
+        KC_QUOT, KC_NUHS,                                     KC_P4,   KC_P5,   KC_P6,
+        KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,
+        KC_SLSH, KC_RSFT,                   KC_UP,            KC_P1,   KC_P2,   KC_P3,   KC_PENT,
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT,
+        KC_RGUI, KC_MENU, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT, KC_P0,            KC_PDOT
     ),
 
 };
 
-*/
 
+/*
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_LPRN, KC_RPRN, KC_PSLS, KC_PAST,
@@ -48,7 +47,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
 };
+*/
 
+/*
 unsigned char amiga_keycode_table[MATRIX_ROWS][MATRIX_COLS] = LAYOUT(
 		AKC_PLP,  AKC_PRP,   AKC_PDV,  AKC_PML,
 		AKC_P7,   AKC_P8,    AKC_P9,   AKC_PMN,
@@ -56,8 +57,8 @@ unsigned char amiga_keycode_table[MATRIX_ROWS][MATRIX_COLS] = LAYOUT(
 		AKC_P1,   AKC_P2,    AKC_P3,   AKC_PEN,
 		AKC_P0,              AKC_PDT
     );
+*/
 
-/*
 unsigned char amiga_keycode_table[MATRIX_ROWS][MATRIX_COLS] = LAYOUT(
 		0x00,             0x00,    0x00,    0x00,    0x00,             0x00,    0x00,    0x00,
         0x00,             0x00,    0x00,    0x00,    0x00,             0x00,    0x00,    0x00,
@@ -72,5 +73,4 @@ unsigned char amiga_keycode_table[MATRIX_ROWS][MATRIX_COLS] = LAYOUT(
         0x00,   0x00,     0x00,    0x00,    0x00,    0x00,   0x00,
         0x00,   0x00,     0x00,    0x00,    0x00,    0x00
     );
-*/
 
